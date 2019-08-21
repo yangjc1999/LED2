eb412fac01849498842f9d154652aa525b15d8a5
yangjc1999 <52656404+yangjc1999@users.noreply.github.com>
2019-08-21 23:10:00 +0800

    c

---
+++ key.c
@@ -0,0 +1,40 @@
+#include "stm32f10x.h"
+#include "key.h"
+#include "sys.h" 
+#include "delay.h"
+
+void KEY_Init(void) //IO��ʼ��
+{ 
+ 	GPIO_InitTypeDef GPIO_InitStructure;
+ 
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOE,ENABLE);//ʹ��PORTA,PORTEʱ��
+
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4;//KEY0-KEY2
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //���ó���������
+ 	GPIO_Init(GPIOE, &GPIO_InitStructure);//��ʼ��GPIOE2,3,4
+
+
+}
+//������������
+//���ذ���ֵ
+//mode:0,��֧��������;1,֧��������;
+//0��û���κΰ�������
+//1��KEY0����
+//2��KEY1����
+//3��KEY2���� 
+//4��KEY3���� WK_UP
+//ע���˺�������Ӧ���ȼ�,KEY0>KEY1>KEY2>KEY3!!
+u8 KEY_Scan(u8 mode)
+{	 
+	static u8 key_up=1;//�������ɿ���־
+	if(mode)key_up=1;  //֧������		  
+	if(key_up&&(KEY0==0||KEY1==0||KEY2==0))
+	{
+		delay_ms(10);//ȥ���� 
+		key_up=0;
+		if(KEY0==0)return KEY0_PRES;
+		else if(KEY1==0)return KEY1_PRES;
+		else if(KEY2==0)return KEY2_PRES;
+	}else if(KEY0==1&&KEY1==1&&KEY2==1)key_up=1; 	    
+ 	return 0;// �ް�������
+}
